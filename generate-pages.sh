2d9539c30f55364742cc49e8af63a2a1d4c8b346
Charles Flèche <charles.fleche@free.fr>
2021-06-12 16:12:14 -0400

    Fix LDT opening hours

--- generate-pages.sh
+++ generate-pages.sh
@@ -2,17 +2,17 @@
 
 mkdir -p pages
 
-for i in `seq 1 6 204`
+for i in `seq 1 6 24`
 do
-  am=25
-  bm=25
-  cm=25
-  dm=25
-  em=25
-  fm=25
-  d=200601
-  dfr="Expire le 30 juin 2020"
-  den="Expires 30th of June, 2020"
+  am=20
+  bm=20
+  cm=20
+  dm=20
+  em=20
+  fm=20
+  d=210901
+  dfr="Expire le 15 octobre 2021"
+  den="Expires 15th of Oct., 2021"
   printf -v index "%04d" $i
   printf -v an "$d-%04d" $(($i + 0))
   printf -v bn "$d-%04d" $(($i + 1))
@@ -25,6 +25,9 @@ do
   jq -n   ".am = $am | .an = \"$an\" | .adf = \"$dfr\" | .ade = \"$den\" | .bm = $bm | .bn = \"$bn\" | .bdf = \"$dfr\" | .bde = \"$den\" | .cm = $cm | .cn = \"$cn\" | .cdf = \"$dfr\" | .cde = \"$den\" | .dm = $dm | .dn = \"$dn\" | .ddf = \"$dfr\" | .dde = \"$den\" | .em = $em | .en = \"$en\" | .edf = \"$dfr\" | .ede = \"$den\" | .fm = $fm | .fn = \"$fn\" | .fdf = \"$dfr\" | .fde = \"$den\""  > $json
 done
 
+echo Copying recto.png and verso.png to pages/
+cp -v templates/*png pages
+
 for json in pages/*.json
 do
   svg=${json%%.json}.svg
@@ -36,7 +39,7 @@ do
 done
 
 echo Generating final output pages/rectos.pdf
-pdfunite pages/*.pdf pages/rectos.pdf
+pdfunite pages/pages-*.pdf pages/rectos.pdf
 
 echo Generating final output pages/versos.pdf
 cairosvg --dpi 300 --output-width 3300 --output-height 2550 -o pages/versos.pdf templates/verso.svg
